8894725acca034bbd1773a57e961b6089961dccc
Reginald Erzoah <147485458+ReginaldErzoah@users.noreply.github.com>
2024-04-18 10:52:48 +0100

    Update and rename Ecommerce Webpage AB Testing R Project.Rmd to E-commerce Webpage AB Testing R Project.R

--- E-commerce Webpage AB Testing R Project.R
+++ E-commerce Webpage AB Testing R Project.R
@@ -1,54 +1,43 @@
-Install & load 'diplyr' for data manipulation & 'ggplot2' for visualization
+#Install & load 'diplyr' for data manipulation & 'ggplot2' for visualization
 
-```         
 install.packages(c("dplyr","ggplot2"))
 library(dplyr)
 library(ggplot2)
-```
 
-Import CSV data file into R
+#Import CSV data file into R
 
-```{r}
+{r}
 data <- read.csv("C:/Users/THE SKRYBLA/Desktop/Media/PROJECTS/R Projects/ab_data.csv")
-```
 
-Explore data before A/B Testing
+#Explore data before A/B Testing
 
-```{r}
+{r}
 head(data)
 summary(data)
 
-```
 
-Find Conversion rates per group
+#Find Conversion rates per group
 
-```{r}
+{r}
  conversion_rates <- data %>%
-+ group_by(group) %>%
-+ summarise(conversion_rates = mean(converted) * 100)
-
+ group_by(group) %>%
+ summarise(conversion_rates = mean(converted) * 100)
 print(conversion_rates)
-```
 
-Visualize Conversion Rates using bar plot
+#Visualize Conversion Rates using bar plot
 
-```{r}
+{r}
 ggplot(conversion_rates, aes(x=group,y=conversion_rates,fill=group))+geom_bar(stat="identity") + labs(title="Conversion Rates by Group",x="Group",y="Conversion Rate(%)")
-```
 
-Use Chi-square to compare the conversion rates.
+#Use Chi-square to compare the conversion rates.
 
-Create a contingency table
+#Create a contingency table
 
-```{r}
+{r}
 contingency_table <- table(data$group, data$converted)
-```
 
-Perform Chi-square test
+#Perform Chi-square test
 
-```{r}
+{r}
 test_result<-chisq.test(contingency_table)
 print(test_result)
-```
-
-
